6288a6c22acdbb343f340c3301485662a33fb50b
moon-zzyy <gracemjy@konkuk.ac.kr>
2023-05-22 23:03:07 +0900

    two pointers

--- Project1/Project1/20_two_pointers/boj1806.cpp
+++ Project1/Project1/20_two_pointers/boj1806.cpp
@@ -7,17 +7,17 @@ int answer = INT32_MAX;
 int n, s;
 int arr[100001];
 int main(void) {
-  ios::sync_with_stdio(0);
-  cin.tie(0);
-  cin >>n >>s;
-  for(int i=0; i<n;i++)
-	cin >> arr[i];
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cin >> n >> s;
+	for (int i = 0; i < n; i++)
+		cin >> arr[i];
 
-	int st=0, en=0;
-	int res=arr[0];
-	while(st<=en && en<n){
-		if (res>=s){
-			answer = min(answer, en-st+1);
+	int st = 0, en = 0;
+	int res = arr[0];
+	while (st <= en && en < n) {
+		if (res >= s) {
+			answer = min(answer, en - st + 1);
 			res -= arr[st++];
 			continue;
 		}
